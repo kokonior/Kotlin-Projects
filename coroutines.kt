4cbc45e28ed1a13975bdd6163d9ed62a09d486f1
Labib Hiba <57593172+lalabib@users.noreply.github.com>
2021-10-03 16:23:38 +0700

    Change & Update Code

--- coroutines.kt
+++ coroutines.kt
@@ -1,22 +1,23 @@
+
 // kotlin coroutines
 import kotlinx.coroutines.*
 
-suspend fun sum(valueA: Int, valueB: Int): Int {
-            delay(1000L)
-    return valueA + valueB
+suspend fun sum(valueA: Int, valueB: Int, valueC: Int): Int {
+            delay(3000L)
+    return valueA + valueB * valueC
 }
 
-suspend fun multiple(valueA: Int, valueB: Int): Int {
+suspend fun multiple(valueA: Int, valueB: Int, valueC: Int): Int {
             delay(2000L)
-    return valueA * valueB
+    return valueA * valueB - valueC 
 }
 
 fun main() = runBlocking {
 
     println("Counting...")
 
-    val resultSum = async { sum(20, 30) }
-    val resultMultiple = async { multiple(20, 40) }
+    val resultSum = async { sum(30, 20, 20) }
+    val resultMultiple = async { multiple(40, 20, 40) }
 
     println("Result sum: ${resultSum.await()}")
     println("Result multiple: ${resultMultiple.await()}")
